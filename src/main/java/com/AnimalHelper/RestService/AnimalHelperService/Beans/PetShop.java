84495eb35c768e254065e8505f61a82b2211c255
Ashirvad Desai <Ashirvad Desai@LAPTOP-KQG2PDH8>
2019-11-10 14:27:30 +0530

    Basic commit

--- src/main/java/com/AnimalHelper/RestService/AnimalHelperService/Beans/PetShop.java
+++ src/main/java/com/AnimalHelper/RestService/AnimalHelperService/Beans/PetShop.java
@@ -5,11 +5,11 @@ public class PetShop {
 	 String field1;
 	 String field2;
 	 String field3;
-	public PetShop(String field1, String field2, String field3) {
+	public PetShop(String field1, String field2, String field4) {
 		super();
 		this.field1 = field1;
 		this.field2 = field2;
-		this.field3 = field3;
+		this.field3 = field4;
 	}
 	public String getField1() {
 		return field1;
